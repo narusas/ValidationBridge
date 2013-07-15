4ee29a21bdcbf3f7be1e29d7b0b8c5cd3ea050bd
narusas <narusas@gmail.com>
2013-07-15 17:48:05 +0900

    find bug  로  no bug

--- ValidationBridge/src/test/java/net/narusas/jstl/validate/Rule11.java
+++ ValidationBridge/src/test/java/net/narusas/jstl/validate/Rule11.java
@@ -1,12 +1,15 @@
 package net.narusas.jstl.validate;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
-public class Rule11 {
+public class Rule11 implements Serializable{
+	private static final long serialVersionUID = -5825584600198309868L;
+
 	@Min(1000)
 	int no;
 	
--- ValidationBridge/src/test/java/net/narusas/jstl/validate/Rule12.java
+++ ValidationBridge/src/test/java/net/narusas/jstl/validate/Rule12.java
@@ -1,10 +1,13 @@
 package net.narusas.jstl.validate;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.validation.constraints.NotNull;
 
-public class Rule12 {
+public class Rule12 implements Serializable{
+	private static final long serialVersionUID = 6054311979032278711L;
+
 	@NotNull
 	String name;
 
